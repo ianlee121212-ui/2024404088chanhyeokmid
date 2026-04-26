3c9e29ca9cc66875084268c0fb77b6db7e64ee82
ianlee121212-ui <ianlee121212@gmail.com>
2026-04-26 12:52:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ if not st.session_state.login:
     st.header('인증')
     col1, col2 = st.columns(2)
     with col1:
-        user_id = st.text_input('아이디 (힌트: user)')
+        id = st.text_input('아이디 (힌트: user)')
     with col2:
-        user_pw = st.text_input('비밀번호 (힌트: 1111 + 333)')
+        pw = st.text_input('비밀번호 (힌트: 1111 + 333)')
 
     if st.button('로그인'):
-        if user_id == 'user' and user_pw == '1444':
+        if id == 'user' and pw == '1444':
             st.session_state.login = True
         else:
             st.error('인증 실패')
@@ -42,6 +42,12 @@ else:
         st.header('끝')
         final = st.session_state.score
         st.metric(label="최종 점수", value=f"{final}점", delta="완료")
+        
+        if session_state.core == 7:
+                    if st.button("다시하기"):
+                        st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', caption='Streamlit 로고', width=300)
+
+            
         
         if st.button("다시하기"):
             st.session_state.step = 0
