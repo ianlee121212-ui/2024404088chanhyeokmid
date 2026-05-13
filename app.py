68340f23010c74ab45fa623a9e791f374ef88610
ianlee121212-ui <ianlee121212@gmail.com>
2026-05-13 12:06:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ st.title('**중간고사 대체 과제**')
 st.header('숫자야구 + 마피아 / + 15 = 해방')
 st.info('학번: 2024404088 / 이름: 이찬혁')
 
-print("!!! TERMINAL LOG TEST START !!!", flush=True)
-st.write("Browser App Start")
 print("시작")
 
 @st.cache_data
